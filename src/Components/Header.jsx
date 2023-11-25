9c680ec9684229d490a9fd730109b5f44c295a8a
Vincent Le <vkl2109@columbia.edu>
2023-11-25 10:09:21 -0800

    Added links

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,14 +3,43 @@ import {
     rem,
     Image,
     Menu,
-    Burger
+    Burger,
+    Text
 } from "@mantine/core"
 import VINNI from "../assets/VINNI.png"
-import { Link } from "react-router-dom"
+import { useState, useEffect } from 'react';
+import { useLocation, Link } from "react-router-dom"
 import { useDisclosure } from '@mantine/hooks';
+
+const links = [
+  { link: '/music', label: 'Music' },
+  { link: '/game', label: 'Game' },
+  { link: '/portfolio', label: 'Portfolio' }
+];
 
 export function Header () {
     const [opened, { toggle }] = useDisclosure(false);
+    const [active, setActive] = useState(links[0].link);
+    const location = useLocation();
+
+    useEffect(() => {
+        if (location.pathname == '/') setActive()
+    }, [location])
+
+    const items = links.map((link) => (
+        <Link
+        key={link.label}
+        to={link.link}
+        style={{ textDecoration: 'none' }}
+        onClick={() => {
+            setActive(link.link)
+        }}
+        >
+            <Text size="xl" td={active == link.link ? "underline" : null}>
+                {link.label}
+            </Text>
+        </Link>
+    ));
 
     return(
         <Group h={rem(100)} justify="space-between" p="sm">
@@ -18,7 +47,7 @@ export function Header () {
                 <Image src={VINNI} h="100%" />
             </Link>
             <Group gap={20} visibleFrom="sm">
-
+                {items}
             </Group>
             <Menu opened={opened} onChange={toggle}>
           <Menu.Target>
