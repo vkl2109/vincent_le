4a8af3bca073ef986ca9a7f3a23f061f737bdef0
Vincent Le <vkl2109@columbia.edu>
2023-11-27 12:17:30 -0500

    added email button

--- src/Routes/LandingPage.jsx
+++ src/Routes/LandingPage.jsx
@@ -4,6 +4,8 @@ import {
     Avatar,
     Title,
     Container,
+    Stack,
+    Button,
     rem
 } from "@mantine/core"
 import { useDisclosure } from "@mantine/hooks"
@@ -15,45 +17,48 @@ import classes from "./LandingPage.module.css"
 export function LandingPage () {
     const [ opened, { toggle }] = useDisclosure(false)
 
+    const handleClick = () => {
+        window.open("mailto:vincentle2021@gmail.com")
+    }
+
     return(
         <BackgroundImage 
             src={waves}
             h={"calc(100vh - var(--app-shell-header-height, 0px) - var(--app-shell-footer-height, 0px)"}
             >
-            <Container
+            <Group
                 w="100%"
                 h="100%"
                 class={classes.backgroundWaves}
                 >
-                <Group
-                    justify="center"
-                    align="center"
-                    gap={rem(50)}
-                    wrap
-                    >
-                    <Container 
-                        h={rem(200)}
-                        w={rem(200)}
-                        style={{ perspective: rem(1000) }}>
-                        <Container class={classes.innerCard} 
-                        onClick={toggle}
-                        style={{ transform: opened ? "rotateY(360deg)" : "rotateY(0deg)"}}>
-                            <Avatar 
-                                src={vincentle}
-                                class={classes.avatar}
-                                />
-                            <Avatar 
-                                src={logo}
-                                p="xl"
-                                class={classes.avatarBack}
-                                />
-                        </Container>
+                <Container 
+                    style={{ perspective: rem(1000) }}>
+                    <Container class={classes.innerCard} 
+                    onClick={toggle}
+                    style={{ transform: opened ? "rotateY(360deg)" : "rotateY(0deg)"}}>
+                        <Avatar 
+                            src={vincentle}
+                            class={classes.avatar}
+                            />
+                        <Avatar 
+                            src={logo}
+                            p="xl"
+                            class={classes.avatarBack}
+                            />
                     </Container>
-                    <Title order={1} size={rem(50)}>
-                        Hi, I'm Vincent!
-                    </Title>
-                </Group>
-            </Container>
+                </Container>
+                <Title order={1} mt="lg">
+                    Hi, I'm Vincent!
+                </Title>
+                <Title size={rem(50)} m="lg">
+                    Full Stack Mobile App & Web Developer
+                </Title>
+                <Button 
+                onClick={handleClick}
+                size="xl" variant="filled" radius="xl" bg="white" c="black">
+                    Work With Me
+                </Button>
+            </Group>
         </BackgroundImage>
     )
 }
