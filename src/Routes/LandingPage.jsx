17dd10ab48ff747278f4aae89e25ada1560a89a1
Vincent Le <vkl2109@columbia.edu>
2023-11-27 13:44:59 -0500

    Added hyve info card

--- src/Routes/LandingPage.jsx
+++ src/Routes/LandingPage.jsx
@@ -6,13 +6,20 @@ import {
     Container,
     Stack,
     Button,
-    rem
+    rem,
+    Badge,
+    Card,
+    Image,
+    Pill,
+    Text,
+    Divider
 } from "@mantine/core"
 import { useDisclosure } from "@mantine/hooks"
 import waves from "../assets/waves.svg"
 import vincentle from "../assets/vincentle.png"
 import logo from "../assets/logo.png"
 import classes from "./LandingPage.module.css"
+
 
 export function LandingPage () {
     const [ opened, { toggle }] = useDisclosure(false)
@@ -21,7 +28,7 @@ export function LandingPage () {
         window.open("mailto:vincentle2021@gmail.com")
     }
 
-    return(
+    return(<>
         <BackgroundImage 
             src={waves}
             h={"calc(100vh - var(--app-shell-header-height, 0px) - var(--app-shell-footer-height, 0px)"}
@@ -48,7 +55,7 @@ export function LandingPage () {
                     </Container>
                 </Container>
                 <Title order={1} mt="lg">
-                    Hi, I'm Vincent!
+                    {"Hi, I'm Vincent!"}
                 </Title>
                 <Title size={rem(50)} m="lg">
                     Full Stack Mobile App & Web Developer
@@ -60,5 +67,50 @@ export function LandingPage () {
                 </Button>
             </Group>
         </BackgroundImage>
-    )
+        <Stack h={"calc(100vh - var(--app-shell-header-height, 0px) - var(--app-shell-footer-height, 0px)"}
+            justify="start"
+            align="center"
+            p="xl">
+            <Badge size={rem(30)} p="xl">My Work</Badge>
+            <Divider my="sm" color="white" w="90vw" />
+            <Group gap="xl" wrap>
+                <Card padding="lg" radius="md" withBorder w={rem(400)}>
+                    <Card.Section>
+                        <Image
+                        src="https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
+                        height={160}
+                        alt="Norway"
+                        />
+                    </Card.Section>
+                    <Card.Section p="md" withBorder justify="center" align="center">
+                        <Group gap="lg" justify="center" align="center" wrap>
+                            <Title>Hyve</Title>
+                            <Badge variant="outline" size="lg">{"Fund Events With Friends"}</Badge>
+                        </Group>
+                        <Text fz="md" c="dimmed" fw={300} lineClamp={4}>
+                            A new payments platform designed to help you create social experiences with your friends. Send invites, collect money upfront, and withdraw funds all in one place.
+                        </Text>
+                        <Group gap="sm" wrap p="sm"  justify="center">
+                            <Pill size="lg">React Native</Pill>
+                            <Pill size="lg">Firebase</Pill>
+                            <Pill size="lg">Expo Go</Pill>
+                        </Group>
+                    </Card.Section>
+                    <Card.Section>
+                        <Button variant="light" color="blue" m="md" w="40%" radius="md"
+                        onClick={() => window.open("http://joinhyve.io/")}
+                        >
+                            Landing Page
+                        </Button>
+                        <Button 
+                        variant="light" color="blue" m="md" w="40%" radius="md"
+                        onClick={() => window.open("https://apps.apple.com/us/app/hyve-fund-events-with-friends/id6449512036")}
+                        >
+                            App Store
+                        </Button>
+                    </Card.Section>
+                </Card>
+            </Group>
+        </Stack>
+    </>)
 }
